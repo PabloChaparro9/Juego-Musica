7c1eb7d03cac30ab490a9d9fc961ba38d1812c97
Pablo Chaparro <pablo.47.114@gmail.com>
2026-05-25 20:51:28 -0300

    Se crea funcion de eleccion de intervalos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const SelectEscalaHTML = document.getElementById("selectEscala");
 const NotaAcordeHTML = document.getElementById("notaAcorde");
+let intervalos = ['C','D','E','F','G','A','B'];
 const CambiarEscala = () => {
     if(SelectEscalaHTML.innerHTML=="Escala Mayor"){
         SelectEscalaHTML.innerHTML = "Escala Menor";
@@ -15,5 +16,108 @@ const CambiarNotaAcorde = () => {
     }
 }
 const CambiarGrado = (a) =>{
-    console.log('grado'+a)
+    let b;
+    let nota;
+    b = DetectarIntervalo(a);
+    nota = DefinirNota(a);
+    const CambiarAcordeHTML = document.getElementById(b);
+    CambiarAcordeHTML.innerHTML = nota;
+}
+const DefinirNota = (a) => {
+    switch (a) {
+        case 'Im':
+            return intervalos[0]
+            break;
+        case 'IM':
+            return intervalos[0]+'#'
+            break;
+        case 'IIm':
+            return intervalos[1]
+            break;
+        case 'IIM':
+            return intervalos[1]+'#'
+            break;
+        case 'IIIm':
+            return intervalos[2]
+            break;
+        case 'IIIM':
+            return intervalos[2]+'#'
+            break;
+        case 'IVm':
+            return intervalos[3]
+            break;
+        case 'IVM':
+            return intervalos[3]+'#'
+            break;
+        case 'Vm':
+            return intervalos[4]
+            break;
+        case 'VM':
+            return intervalos[4]+'#'
+            break;
+        case 'VIm':
+            return intervalos[5]
+            break;
+        case 'VIM':
+            return intervalos[5]+'#'
+            break;
+        case 'VIIm':
+            return intervalos[6]
+            break;
+        case 'VIIM':
+            return intervalos[6]+'#'
+            break;
+    
+        default:
+            break;
+    }
+}
+const DetectarIntervalo = (c) =>{
+    switch (c) {
+        case 'Im':
+            return 'gradoI'
+            break;
+        case 'IM':
+            return 'gradoI'
+            break;
+        case 'IIm':
+            return 'gradoII'
+            break;
+        case 'IIM':
+            return 'gradoII'
+            break;
+        case 'IIIm':
+            return 'gradoIII'
+            break;
+        case 'IIIM':
+            return 'gradoIII'
+            break;
+        case 'IVm':
+            return 'gradoIV'
+            break;
+        case 'IVM':
+            return 'gradoIV'
+            break;
+        case 'Vm':
+            return 'gradoV'
+            break;
+        case 'VM':
+            return 'gradoV'
+            break;
+        case 'VIm':
+            return 'gradoVI'
+            break;
+        case 'VIM':
+            return 'gradoVI'
+            break;
+        case 'VIIm':
+            return 'gradoVII'
+            break;
+        case 'VIIM':
+            return 'gradoVII'
+            break;
+    
+        default:
+            break;
+    }
 }
